9d629d4f14b51f6bdf8a947dd9a09f852a84feae
RahmaBasly <35639830+RahmaBasly@users.noreply.github.com>
2018-10-21 21:36:08 +0100

    packages

---
+++ Map-JavaEE-ejb/src/main/java/tn/esprit/Map/utils/RestActivator.java
@@ -0,0 +1,9 @@
+package tn.esprit.Map.utils;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class RestActivator  extends Application{
+
+}
