4ae15b9a03c11f404d98e9a9c887c701929805c4
Mimoun Majd <majdmn6@gmail.com>
2018-10-23 21:13:23 +0100

    cv achref et toi?

---
+++ Map-JavaEE-ejb/src/main/java/tn/esprit/Map/interfaces/JobRequestLocal.java
@@ -0,0 +1,18 @@
+package tn.esprit.Map.interfaces;
+
+import javax.ejb.Local;
+import javax.ejb.Remote;
+
+import tn.esprit.Map.persistences.JobRequest;
+@Local
+public interface JobRequestLocal {
+
+	public String AddJobRequest(JobRequest jobRequest);
+	public String UpdateJobRequest(int id);
+	public String DeleteJobRequest(int id);
+	
+	
+	
+}
+
+
