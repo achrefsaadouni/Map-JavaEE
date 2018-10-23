3131acfbff392127510a2a75841dc016f32390d6
Achref <achref.saadouni@esprit.tn>
2018-10-23 22:24:29 +0100

    d

--- Map-JavaEE-ejb/src/main/java/tn/esprit/Map/interfaces/JobRequestLocal.java
+++ Map-JavaEE-ejb/src/main/java/tn/esprit/Map/interfaces/JobRequestLocal.java
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-package tn.esprit.Map.interfaces;
-
-import javax.ejb.Local;
-import javax.ejb.Remote;
-
-import tn.esprit.Map.persistences.JobRequest;
-@Local
-public interface JobRequestLocal {
-	public String AddJobRequest(JobRequest jobRequest);
-	public String UpdateJobRequest(int id);
-	public String DeleteJobRequest(int id);
-	public String ViewAllRequested();
-}
-
-
-=======
 package tn.esprit.Map.interfaces;
 
 import javax.ejb.Local;
@@ -33,5 +16,3 @@ public interface JobRequestLocal {
 	
 }
 
-
->>>>>>> master
