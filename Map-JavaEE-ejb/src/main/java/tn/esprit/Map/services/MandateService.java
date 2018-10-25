845bc987ccc7d2afa2eb9ee001757c5a62bc375a
Emir <Emir@DESKTOP-OC4N03B>
2018-10-25 02:11:14 +0100

    synchronisation

--- Map-JavaEE-ejb/src/main/java/tn/esprit/Map/services/MandateService.java
+++ Map-JavaEE-ejb/src/main/java/tn/esprit/Map/services/MandateService.java
@@ -11,6 +11,7 @@ import javax.persistence.TypedQuery;
 
 import tn.esprit.Map.interfaces.MandateServiceLocal;
 import tn.esprit.Map.persistences.Mandate;
+import tn.esprit.Map.persistences.Request;
 import tn.esprit.Map.persistences.Resource;
 import tn.esprit.Map.persistences.Skill;
 
@@ -105,14 +106,16 @@ public class MandateService implements MandateServiceLocal {
 
 	}
 
+	
+
 	@Override
-	public boolean addMandate(int ressourceId,int projetId,Date dateDebut,Date dateFin) {
-		
+	public boolean addGps(int ressourceId,int projetId,Date dateDebut,Date dateFin, int gpsId) {
 		return false;
 	}
 
 	@Override
-	public boolean addGps(int ressourceId,int projetId,Date dateDebut,Date dateFin, int gpsId) {
+	public boolean addMandate(Request request) {
+		// TODO Auto-generated method stub
 		return false;
 	}
 
