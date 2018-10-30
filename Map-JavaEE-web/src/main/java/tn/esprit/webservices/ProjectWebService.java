c8e033eba40e17367f2a9099ba0f94c5d53b3c85
RahmaBasly <35639830+RahmaBasly@users.noreply.github.com>
2018-10-30 17:12:56 +0100

    ServiceWeb rahma

--- Map-JavaEE-web/src/main/java/tn/esprit/webservices/ProjectWebService.java
+++ Map-JavaEE-web/src/main/java/tn/esprit/webservices/ProjectWebService.java
@@ -6,12 +6,14 @@ import java.util.Date;
 
 import javax.annotation.ManagedBean;
 import javax.ejb.EJB;
+import javax.ejb.Schedule;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import tn.esprit.Map.interfaces.ProjectRemote;
 import tn.esprit.Map.persistences.Client;
+import tn.esprit.Map.persistences.Person;
 import tn.esprit.Map.persistences.Project;
 import tn.esprit.Map.persistences.ProjectType;
 
@@ -20,63 +22,78 @@ import tn.esprit.Map.persistences.ProjectType;
 public class ProjectWebService {
 	@EJB
 	ProjectRemote projectRemote;
-	Project project = new Project();
+
+	
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getProjects() {
-		if (projectRemote.getAllProjects() == null)
-			return Response.status(Response.Status.NOT_FOUND).build();
+	public Response getProjects(@QueryParam("idClient") String idClient,@QueryParam("startDate") String startDate,@QueryParam("endDate") String endDate) throws ParseException {
+		
+		if ((idClient == null)&&(startDate==null)&&(endDate ==null)) {
+			if (projectRemote.getAllProjects() == null)
+				return Response.status(Response.Status.NOT_FOUND).entity("\"Access denied\"").build();
 
-		if (projectRemote.getAllProjects().size() == 0)
-			return Response.status(Response.Status.BAD_REQUEST).entity("No data").build();
+			if (projectRemote.getAllProjects().size() == 0)
+				return Response.status(Response.Status.BAD_REQUEST).entity("\"No data\"").build();
 
-		else
-			return Response.ok(projectRemote.getAllProjects(), MediaType.APPLICATION_JSON).build();
+			else
+				return Response.ok(projectRemote.getAllProjects(), MediaType.APPLICATION_JSON).build();
+		} else if((idClient != null)&&(startDate==null)&&(endDate ==null) ) {
+			if (projectRemote.getAllProjectByClient(Integer.parseInt(idClient)) == null)
+				return Response.status(Response.Status.NOT_FOUND).build();
+
+			if (projectRemote.getAllProjectByClient(Integer.parseInt(idClient)).size() == 0)
+				return Response.status(Response.Status.BAD_REQUEST).entity("\"No data\"").build();
+
+			else
+				return Response.ok(projectRemote.getAllProjectByClient(Integer.parseInt(idClient)), MediaType.APPLICATION_JSON).build();
+		}
+		else /*if((idClient == null)&&(startDate!=null)&&(endDate !=null))*/{
+			if (projectRemote.getProjectsByDate(startDate,endDate) == null)
+				return Response.status(Response.Status.NOT_FOUND).build();
+
+			if (projectRemote.getProjectsByDate(startDate,endDate).size() == 0)
+				return Response.status(Response.Status.BAD_REQUEST).entity("No data").build();
+
+			else
+				return Response.ok(projectRemote.getProjectsByDate(startDate,endDate), MediaType.APPLICATION_JSON).build();
+		}
 
 	}
-
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("{idClient}")
-	public Response getProjectsByClient(@PathParam("idClient") String idClient) {
-		if (projectRemote.getAllProjectByClient(Integer.parseInt(idClient)) == null)
-			return Response.status(Response.Status.NOT_FOUND).build();
-
-		if (projectRemote.getAllProjectByClient(Integer.parseInt(idClient)).size() == 0)
-			return Response.status(Response.Status.BAD_REQUEST).entity("No data").build();
-
-		else
-			return Response.ok(projectRemote.getAllProjectByClient(Integer.parseInt(idClient)), MediaType.APPLICATION_JSON).build();
-
-	}	
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/calculMontant")
+	public String getsumAmountProjects(@QueryParam("startDate") String startDate,@QueryParam("endDate") String endDate)
+	{
+		return projectRemote.sumAmountProject(startDate, endDate);
+	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public int addProject(Project project) {	
-		int projectId = projectRemote.addProject(project);
-		return projectId;
+	public String postProject(@QueryParam("idClient") String idClient, @QueryParam("idProject") String idProject,
+			Project project) throws ParseException {
+		if ((idClient == null) && (idProject == null)) {
+			return projectRemote.addProject(project);
+		} else {
+			return projectRemote.assignProjectToClient(Integer.parseInt(idClient), Integer.parseInt(idProject));
+		}
 	}
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.TEXT_PLAIN)
-	@Path("{idProject}")
-	public String assignProjectToClient(@PathParam("idProject") String idProject,Client client) {
-		 return projectRemote.assignProjectToClient(Integer.parseInt(idProject), client);
 
-	}
+	
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_PLAIN)
-	public String updateProject(Project project){
+	public String updateProject(Project project) {
 		return projectRemote.updateProject(project);
 	}
+
+	
 	@DELETE
 	@Produces(MediaType.TEXT_PLAIN)
 	@Path("{idProject}")
-	public String deleteProject(@PathParam("idProject") String idProject){
+	public String deleteProject(@PathParam("idProject") String idProject) {
 		return projectRemote.deleteProject(Integer.parseInt(idProject));
 	}
-	
+
 }
